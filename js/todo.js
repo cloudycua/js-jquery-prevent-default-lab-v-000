28e3204908fbd77e2b25ebe113fc873fb6202c75
Christine Cua <cloudycua@gmail.com>
2019-02-06 20:40:02 +0000

    Automatically backed up by Learn

--- js/todo.js
+++ js/todo.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 
 function submitForm() {
   $('form').on('submit', function(event) {
-
+    var item = $('#item').val()
+    $("#list ul").append('<li><a href="/user/messages"><span class="tab">Message Center</span></a></li>')
   })
 }
